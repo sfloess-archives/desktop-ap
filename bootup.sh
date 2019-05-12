6b459c799ecc35b5ed17e05242420e549c8198ff
Scot P. Floess <sfloess@nc.rr.com>
2019-05-12 15:13:14 -0400

    NA - fixing mount points

--- bootup.sh
+++ bootup.sh
@@ -2,13 +2,11 @@
 
 debdir=/mnt/sda1/debian_armel
 
-mkdir -p $debdir/proc $debdir/sys $debdir/dev $debdir/dev/pts $debdir/tmp
+mkdir -p $debdir/proc $debdir/sys $debdir/dev $debdir/dev/pts $debdir/tmp $debdir/mnt/desktop-ap $debdir/lib/modules/`uname -r`
 
 cat /etc/mtab > $debdir/etc/fstab
 
 # ----------------------------------------------------------------------
-
-#chmod -R 755 $debdir
 
 chmod    700 $debdir/etc/ssh
 chmod -R 700 $debdir/etc/ssh
@@ -17,15 +15,18 @@ rm -rf $debdir/run/screen/*
 
 # ----------------------------------------------------------------------
 
-mount -t devpts none  $debdir/dev/pts
+mount -t devpts none                  $debdir/dev/pts
 
-mount -o bind   /     $debdir/mnt/desktop-ap
-mount -o bind   /dev  $debdir/dev
-mount -o bind   /proc $debdir/proc
-mount -o bind   /sys  $debdir/sys
-mount -o bind   /tmp  $debdir/tmp
+mount -o bind /                       $debdir/mnt/desktop-ap
+mount -o bind /dev                    $debdir/dev
+mount -o bind /proc                   $debdir/proc
+mount -o bind /sys                    $debdir/sys
+mount -o bind /tmp                    $debdir/tmp
+
+mount -o bind /lib/modules/`uname -r` $debdir/lib/modules/`uname -r`
 
 # ----------------------------------------------------------------------
 
 /bin/busybox chroot $debdir $* & 
 
+exit $?
